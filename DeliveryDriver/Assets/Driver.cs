7dae95acfd93898759902d952bb8de1b6680b0b2
nbuzzerio <nick.buzzerio@hotmail.com>
2024-06-03 22:58:41 -0400

    added variables for car object movement

--- DeliveryDriver/Assets/Driver.cs
+++ DeliveryDriver/Assets/Driver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
+    float turnSpeed = 0.1f;
+    float moveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
+        transform.Rotate(0, 0, turnSpeed);
+        transform.Translate(moveSpeed, moveSpeed, 0);
         
     }
 }
